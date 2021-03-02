c3ea55400904858d51da226ebe7dabf051cc54fc
Michael Sonnino <monnino@gmail.com>
2021-03-02 12:59:57 +0200

    Add camera and photo library functionality

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -6,14 +6,58 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: nil, message: nil,
+                               preferredStyle: .actionSheet)
 
+          let actCancel = UIAlertAction(title: "Cancel", style: .cancel,
+                                      handler: nil)
+          alert.addAction(actCancel)
 
+          let actPhoto = UIAlertAction(title: "Take Photo",
+                                       style: .default) { (_) in
+            self.imagePicker.sourceType = .camera
+            self.present(self.imagePicker, animated: true, completion: nil)
+          }
+          alert.addAction(actPhoto)
+
+          let actLibrary = UIAlertAction(title: "Choose From Library",
+                                         style: .default) { (_) in
+            self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+          }
+          alert.addAction(actLibrary)
+
+          present(alert, animated: true, completion: nil)
+    }
+    
 }
 
