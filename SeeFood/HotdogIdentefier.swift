0d4ac2fdd4311f569d962b0f2b497ceae05d4925
Michael Sonnino <monnino@gmail.com>
2021-03-02 18:10:48 +0200

    Add file HotdogIdentifier Hotdog identifying functionality complete. Refactor code

---
+++ SeeFood/HotdogIdentefier.swift
@@ -0,0 +1,49 @@
+//
+//  hotdogIdentifier.swift
+//  SeeFood
+//
+//  Created by Michael Sonnino on 02/03/2021.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+struct HotdogIdentefier {
+    
+    func isHotdog(image: CIImage) -> Bool {
+        
+        var isHotdog = false
+        
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: Inceptionv3.urlOfModelInThisBundle)) else {
+            fatalError("Can't load ML model")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let result = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to proccess image")
+            }
+            
+            if let firstResult = result.first {
+                if firstResult.identifier.contains("hotdog") {
+                    isHotdog = true
+                } else {
+                    isHotdog = false
+                }
+            }
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+        return isHotdog
+        
+    }
+    
+}
